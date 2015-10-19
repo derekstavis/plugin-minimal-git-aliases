5f0d155d78a64e1800d1868f05c536005e00bcdf
Derek Stavis <derek@dba.eng.br>
2015-10-19 14:43:59 -0200

    First steps to avoid RSI while using git

---
+++ init.fish
@@ -0,0 +1,12 @@
+function init -a path --on-event init_minimal-git-aliases
+  # Two character series
+  abbr gc git commit
+  abbr gs git status
+  abbr gf git fetch
+  abbr gp git pull
+
+  # Four character series
+  abbr gpsh git push
+  abbr grst git reset
+  abbr grbs git rebase
+end
