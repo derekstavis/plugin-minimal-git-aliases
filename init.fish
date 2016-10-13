ba4b3c2a0001f82abe319a2bac1dab1466ec6387
Derek Willian Stavis <dekestavis@gmail.com>
2016-10-13 12:42:31 -0300

    Lots of unused abbreviations, do some clean up

--- init.fish
+++ init.fish
@@ -1,29 +1,5 @@
-function init -a path --on-event init_minimal-git-aliases
-  # general commands
-  abbr gs git status
-  abbr gf git fetch
-  abbr gp git pull
-  abbr gl git log
-  abbr gr git reset
-
-  # git push
-  abbr gpsh git push
-  abbr gpom git push origin master
-
-  # git commit
-  abbr gc git commit
-  abbr gc-m git commit -m
-  abbr gc-am git commit --am
-
-  # git rebase
-  abbr grb git rebase
-  abbr grb-c git rebase --continue
-  abbr grb-i git rebase --i
-
-  # git remote
-  abbr grt git remote
-  abbr grt-v git remote -v
-  abbr grt-a git remote add
-  abbr grt-r git remote rm
-
-end
+abbr gs git status
+abbr gf git fetch
+abbr gp git pull
+abbr gl git log
+abbr gr git reset
